0987fbd086372d273ae5b4735bea0da59ad04a9d
Rituraj Yadav <rituraj0123210@gmail.com>
2023-02-16 11:07:15 +0530

    button effect and id change in main activity

--- app/src/main/java/com/example/fitnessandyoga/MainActivity.kt
+++ app/src/main/java/com/example/fitnessandyoga/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.fitnessandyoga
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val WhtBtn=findViewById<ImageButton>(R.id.wht_btn)
+        WhtBtn.setOnClickListener {
+            Toast.makeText(this,"Opening White Music",Toast.LENGTH_SHORT).show()
+        }
+
+
+
+
+
+
     }
 }
