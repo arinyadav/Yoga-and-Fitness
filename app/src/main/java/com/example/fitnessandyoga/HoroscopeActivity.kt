5f4b34bb13078c3c91ab9b87fcae5e00ea047de7
arin12345git config  --global user.name arin12345git config  --global user.name arin12345 <arinyadav1908@gmail.com>
2023-02-24 19:08:52 +0530

    number2

---
+++ app/src/main/java/com/example/fitnessandyoga/HoroscopeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fitnessandyoga
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HoroscopeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.horoscope)
+    }
+}
