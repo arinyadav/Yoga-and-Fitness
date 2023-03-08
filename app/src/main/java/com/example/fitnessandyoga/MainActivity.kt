7531f1c9b4baaee52f0f6acee61a8b64d33cadc1
Rituraj Yadav <rituraj0123210@gmail.com>
2023-03-08 20:47:54 +0530

    Layout of yoga

--- app/src/main/java/com/example/fitnessandyoga/MainActivity.kt
+++ app/src/main/java/com/example/fitnessandyoga/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.fitnessandyoga
 
+
+
 import android.content.Intent
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,29 +11,29 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var btn1:ImageButton
-    lateinit var btn2:ImageButton
-    lateinit var btn3:ImageButton
-    lateinit var btn4:ImageButton
+    lateinit var musicbtn:ImageButton
+    lateinit var yogabtn:ImageButton
+    lateinit var motivbtn:ImageButton
+    lateinit var horobtn:ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn1=findViewById(R.id.wht_btn)
-        btn2=findViewById(R.id.yoga_btn)
-        btn3=findViewById(R.id.motiv_btn)
-        btn4=findViewById(R.id.horo_btn)
+        musicbtn=findViewById(R.id.wht_btn)
+        yogabtn=findViewById(R.id.yoga_btn)
+        motivbtn=findViewById(R.id.motiv_btn)
+        horobtn=findViewById(R.id.horo_btn)
 
 
 
-        btn1.setOnClickListener {
+        musicbtn.setOnClickListener {
             Toast.makeText(this,"Opening White Music",Toast.LENGTH_SHORT).show()
 
             val Intent= Intent(this,WhiteMusicActivity::class.java)
             startActivity(Intent)
         }
 
-        btn2.setOnClickListener {
+        horobtn.setOnClickListener {
             Toast.makeText(this,"Opening Horoscope",Toast.LENGTH_SHORT).show()
 
             val Intent= Intent(this,WhiteMusicActivity::class.java)
@@ -40,13 +41,20 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        btn3.setOnClickListener {
+        motivbtn.setOnClickListener {
             Toast.makeText(this,"Opening Motivational Quotes",Toast.LENGTH_SHORT).show()
+
+            val Intent= Intent(this,WhiteMusicActivity::class.java)
+            startActivity(Intent)
+
         }
 
-        btn4.setOnClickListener {
-            Toast.makeText(this,"Opening Yoga",Toast.LENGTH_SHORT).show()
-        }
+        yogabtn.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this, YogaActivity::class.java))
+
+        })
+
+
 
 
     }
--- app/src/main/java/com/example/fitnessandyoga/YogaActivity.kt
+++ app/src/main/java/com/example/fitnessandyoga/YogaActivity.kt
@@ -1,4 +1,34 @@
 package com.example.fitnessandyoga
 
-class YogaActivity {
+import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fitnessandyoga.R
+import com.example.fitnessandyoga.YogaAdapter
+
+class YogaActivity : AppCompatActivity() {
+    private lateinit var listView: ListView
+    private lateinit var title: Array<String>
+    private var animation: Animation? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_yoga)
+        supportActionBar?.hide()
+        listView = findViewById<ListView>(R.id.list_view)
+        title = resources.getStringArray(R.array.title)
+        val adapter = YogaAdapter(this@YogaActivity, title)
+        animation = AnimationUtils.loadAnimation(this, R.anim.animation1)
+        listView.adapter = adapter
+        listView.setOnItemClickListener(object : AdapterView.OnItemClickListener {
+            override fun onItemClick(adapterView: AdapterView<*>, view: View, i: Int, l: Long) {
+                Toast.makeText(applicationContext, "" + title[i], Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
 }
